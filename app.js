28ba749522edce6602285f2d0214cb4ba699deec
Your Name <you@example.com>
2024-03-13 13:50:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+"use strict";
+let personName = '';
+personName = prompt('Whar is your name?') || '';
+let lowercase = personName.toLowerCase();
+let uppercase = personName.toUpperCase();
+let titlecase = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).
+    toLowerCase()).join(' ');
+if (personName !== null && personName !== '') {
+    alert(`Hello ${personName}, Here are your name in:
+    lowercase: ${lowercase}
+    upppercase: ${uppercase}
+    Titlecase:${titlecase}`);
+}
+else {
+    alert('please fill your name !');
+}
---
+++ app.ts
@@ -0,0 +1,18 @@
+let personName : string = '';
+
+ personName = prompt('Whar is your name?')|| '';
+
+let lowercase : string = personName.toLowerCase();
+let uppercase : string = personName.toUpperCase();
+let titlecase : string = personName.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).
+toLowerCase()).join(' ')
+
+if(personName !== null && personName !==''){
+    alert(`Hello ${personName}, Here are your name in:
+    lowercase: ${lowercase}
+    upppercase: ${uppercase}
+    Titlecase:${titlecase}`)
+}
+else{
+    alert('please fill your name !')
+}
