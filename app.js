22f5fe176eed18ee1192c59c4bab1277425be242
Microtechbymak26 <usubka786@Gmail.com>
2024-04-06 11:03:32 -0700

    updated

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 "use strict";
-let personName = "ERIC";
-console.log(`hello ${personName} would you like to learn some python today?`);
+// lower case
+let personName = "Ateeb";
+console.log("lowercase:", personName.toLowerCase());
+// upper case
+console.log("uppercase:", personName.toLocaleUpperCase());
+// Tittle case
+console.log("tittlecase:", personName.replace(/\bw/g, c => c.toUpperCase()));
+// number addition multiplication and %
+let a = 29;
+let b = 30;
+console.log("a * b=", a * b);
